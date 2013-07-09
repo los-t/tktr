f745a949b69a1c6ddc5af6d93643eab849eab11f
los-t <los-t@ya.ru>
2013-07-09 05:12:18 +0400

    Use uniform initalization and scoped C-routines

--- src/cli.cpp
+++ src/cli.cpp
@@ -8,19 +8,19 @@ tktr::Action tktr::CLI::parse_action(const char *src)
 {
 	switch (src[0]) {
 		case 'n':
-			if (!src[1] || !strncmp(src, "new", 3))
+			if (!src[1] || !std::strncmp(src, "new", 3))
 				return tktr::Action::Create;
 			break;
 		case 'c':
-			if (!src[1] || !strncmp(src, "complete", 8))
+			if (!src[1] || !std::strncmp(src, "complete", 8))
 				return tktr::Action::Complete;
 			break;
 		case 's':
-			if (!src[1] || !strncmp(src, "start", 5))
+			if (!src[1] || !std::strncmp(src, "start", 5))
 				return tktr::Action::Start;
 			break;
 		case 'p':
-			if (!src[1] || !strncmp(src, "pause", 5))
+			if (!src[1] || !std::strncmp(src, "pause", 5))
 				return tktr::Action::Pause;
 			break;
 	}
@@ -32,7 +32,7 @@ tktr::CLI::CLI(int argc, const char **argv)
 {
 	switch (argc) {
 		case 3:
-			this->tag_ = std::string(argv[2]);
+			this->tag_ = std::string{argv[2]};
 		case 2:
 			this->act_ = CLI::parse_action(argv[1]);
 			break;
