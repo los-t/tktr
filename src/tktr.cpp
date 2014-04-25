125c1f398a3d9f8ed2a5c7e6b5d56ed2aabd30a7
Vladimir Dranyonkov <v.dranyonkov@gmail.com>
2014-04-25 11:57:55 +0400

    Fix alignment

--- src/tktr.cpp
+++ src/tktr.cpp
@@ -7,13 +7,13 @@
 void print_usage(void)
 {
 	std::cout << "tktr - your simple task tracker" << std::endl
-						<< "Usage: tktr [action] [tag]" << std::endl
-						<< "Tag is the key to find your task" << std::endl
-						<< "Actions:" << std::endl
-						<< "\tn[ew]\t\t- create new task" << std::endl
-						<< "\tc[omplete]\t- complete existing task" << std::endl
-						<< "\ts[tart]\t\t- start a (possibly new) task" << std::endl
-						<< "\tp[ause]\t\t- pause a running task" << std::endl;
+	          << "Usage: tktr [action] [tag]" << std::endl
+	          << "Tag is the key to find your task" << std::endl
+	          << "Actions:" << std::endl
+	          << "\tn[ew]\t\t- create new task" << std::endl
+	          << "\tc[omplete]\t- complete existing task" << std::endl
+	          << "\ts[tart]\t\t- start a (possibly new) task" << std::endl
+	          << "\tp[ause]\t\t- pause a running task" << std::endl;
 }
 
 int main(int argc, const char **argv)
